152f8955cf8e2ec75cdfec0cdc4e28e5b4cf3355
aperezg <perezg.adrian@gmail.com>
2018-03-27 10:31:00 +0200

    Add testing for valid create project

--- project/service_test.go
+++ project/service_test.go
@@ -14,5 +14,14 @@ func TestReturnAnErrorCreatingProject(t *testing.T) {
 	s := NewService(r)
 	_, err := s.CreateProject("test_project")
 	assert.EqualError(t, err, err.Error())
+}
 
+func TestCreateProject(t *testing.T) {
+	r := new(storeProject.RepositoryMock)
+	r.On("CreateProject", "test_project").Return(nil)
+
+	s := NewService(r)
+	p, err := s.CreateProject("test_project")
+	assert.Equal(t, Project{}, p)
+	assert.NoError(t, err)
 }
